34c5fe52f88480bd78f454feaf9c2df7d9bbc4f3
sarathsasilearning <sarath.sasi.unix@gmail.com>
2025-04-08 20:21:54 +0530

    added new code

---
+++ my-static-website/server.js
@@ -0,0 +1,13 @@
+// server.js
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+// Serve static files from the root directory
+app.use(express.static(path.join(__dirname)));
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
